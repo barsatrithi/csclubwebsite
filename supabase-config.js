54aa94d2f66896b055962799b852aa4c44dc88dd
Barsat Rithi <148896258+barsatrithi@users.noreply.github.com>
2026-04-15 09:14:40 -0400

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-window.supabaseConfig = {
-  url: "https://dsvqbtgvnwzehcegljmj.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzdnFidGd2bnd6ZWhjZWdsam1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMDA5MTQsImV4cCI6MjA5MTc3NjkxNH0.rPBkJ7MXOJG5Im7Tfy6qwIlwVlS0UIJU2uL76DVXzZY"
-};
